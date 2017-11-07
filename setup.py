1bc2af8f3e290bc176945905cabca773f11b6b56
Alexander Condello <acondello@dwavesys.com>
2017-11-07 14:44:37 -0800

    Add warning to setup.py until it is implemented

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-
+"""TODO"""
 
 requirements = ['dwave_micro_client>=0.1',
                 'dimod>=0.3.1',
                 'numpy']
+
+raise NotImplementedError("setup has not been implemented yet")
