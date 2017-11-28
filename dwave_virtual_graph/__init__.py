dcece839d100aa20f2a1ce6ee29dfe958bec6f6b
Alexander Condello <acondello@dwavesys.com>
2017-11-28 11:11:18 -0800

    Restart

--- dwave_virtual_graph/__init__.py
+++
@@ -1,5 +0,0 @@
-from dwave_virtual_graph.composite import *
-import dwave_virtual_graph.composite
-
-from dwave_virtual_graph.embedding_utilities import *
-import dwave_virtual_graph.embedding_utilities
