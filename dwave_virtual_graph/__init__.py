15d888208518971c22058597d6df58aa26a24232
Alexander Condello <acondello@dwavesys.com>
2017-12-07 13:32:35 -0800

    Add code to determine the cache location

--- dwave_virtual_graph/__init__.py
+++ dwave_virtual_graph/__init__.py
@@ -1,3 +1,6 @@
+__version__ = '0.1.0'
+
 from dwave_virtual_graph.flux_bias_offsets import *
 from dwave_virtual_graph.embedding import *
 from dwave_virtual_graph.composite import *
+from dwave_virtual_graph.cache_manager import *
---
+++ dwave_virtual_graph/cache_manager.py
@@ -0,0 +1,31 @@
+import os
+
+import homebase
+
+from dwave_virtual_graph import __version__
+
+__all__ = ['cache_file']
+
+APPNAME = 'dwave-virtual-graph'
+APPAUTHOR = 'dwave-systems'
+DATABASENAME = 'cache_%s.db' % __version__
+
+
+def cache_file(app_name=APPNAME, app_author=APPAUTHOR, filename=DATABASENAME):
+    """Returns the filename (including path) for the data cache.
+
+    Args:
+        app_name (str, optional): Default 'dwave-virtual-graph'.
+        app_author (str, optional): Default 'dwave-systems'
+        filename (str, optional): Default 'cache_[version].db' where
+            version is the current version of dwave_virtual_graph.
+
+    Returns:
+        str: The full path to the file that can be used as a cache.
+
+    Notes:
+        Creates the directory if it does not already exist.
+
+    """
+    user_data_dir = homebase.user_data_dir(app_name=APPNAME, app_author=APPAUTHOR, create=True)
+    return os.path.join(user_data_dir, DATABASENAME)
