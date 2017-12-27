0bce6237c26a71a1cbbd4a62c453a7167a866f89
Alexander Condello <acondello@dwavesys.com>
2017-12-27 14:11:53 -0800

    Add minorminer and embedding_utilities dependency

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ else:
 
 install_requires = ['dimod==0.5.0.dev1',
                     'dwave_micro_client==0.1',
-                    'minorminer==0.1.0.dev1']
+                    'minorminer==0.1.0.dev1',
+                    'dwave_embedding_utilities']
 tests_require = ['numpy', 'coverage']
 extras_require = {'all': ['numpy'],
                   'tests': tests_require}
