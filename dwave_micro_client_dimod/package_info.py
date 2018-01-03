d43b676e8c6ac6844e58ad188d65df39fab3b8e0
Alexander Condello <acondello@dwavesys.com>
2018-01-02 16:16:44 -0800

    Update version 0.2.0->0.2.1

--- dwave_micro_client_dimod/package_info.py
+++ dwave_micro_client_dimod/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'dimod wrapper for the D-Wave Micro Client'
