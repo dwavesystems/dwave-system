b0c3aa5b6576ff293d2ba8a204b2b28b99afa626
Alexander Condello <acondello@dwavesys.com>
2018-01-05 10:24:47 -0800

    Remove legacy code

--- dwave_virtual_graph/exceptions.py
+++
@@ -1,6 +0,0 @@
-class MissingEmbedding(Exception):
-    """No embedding saved for the specified tag"""
-
-
-class UniqueEmbeddingTagError(Exception):
-    """Raised when trying to overwrite an embedding without a unique tag."""
