217eae6145d04024d5cbb2f6552ceb0918170932
Bradley Ellert <bellert@dwavesys.com>
2018-01-08 17:30:11 -0800

    Start of something that might actually work

--- dwave_micro_client_dimod/tiling.py
+++ dwave_micro_client_dimod/tiling.py
@@ -3,6 +3,7 @@ TilingComposite
 ==================
 """
 #import itertools
+from math import sqrt, ceil
 
 import dimod
 import dwave_networkx as dnx
@@ -15,14 +16,73 @@ class TilingComposite(dimod.TemplateComposite):
     Args:
         sampler (:class:`dimod.TemplateSampler`):
             A structured dimod sampler to be wrapped.
+        sub_size_m (int): The number of rows in the sub Chimera lattice.
+        sub_size_n (int): The number of columns in the sub Chimera lattice.
+        sub_size_t (int): The size of the shore within each Chimera tile.
 
     """
 
-    def __init__(self, sampler):
+    def __init__(self, sampler, sub_size_m, sub_size_n, size_t):
         # The composite __init__ adds the sampler into self.children
         dimod.TemplateComposite.__init__(self, sampler)
         self._child = sampler  # faster access than self.children[0]
+        tile = dnx.chimera_graph(sub_size_m,sub_size_n,size_t)
         self.structure = (sorted(tile.nodes), sorted(tile.edges), sorted(tile.adjacency()))
+
+        nodes_per_tile = size_t * 2. # IS TILE THE ONLY NAME FOR EACH BIPARTITE GRAPH IN CHIMERA LATTICE?
+        size_m = size_n = int(ceil(sqrt(ceil(len(sampler.structure[0]) / nodes_per_tile))))  # CAN SQUARE SHAPE BE ASSUMED?system = dnx.chimera_graph()
+        system = dnx.chimera_graph(size_m, size_n, size_t, sampler.structure[0], sampler.structure[1])
+        c2i = {chimera_index: linear_index for (linear_index, chimera_index) in system.nodes(data='chimera_index')}
+
+        # Count the connections between these qubits
+        def _between(qubits1, qubits2):
+            edges = [edge for edge in system.edges if edge[0] in qubits1 and edge[1] in qubits2]
+            return len(edges)
+
+        # Get the list of qubits in a cell
+        def _cell_qubits(ii, jj):
+            return [c2i((ii, jj, uu, kk)) for uu in range(2) for kk in range(size_t)]
+
+        # get a mask of complete cells
+        cells = [[False for _ in range(size_n)] for _ in range(size_m)]
+        for ii in range(size_m):
+            for jj in range(size_n):
+                qubits = _cell_qubits(ii, jj)
+                active_in_cell = sum(q in system.nodes for q in qubits)
+                cells[ii][jj] = active_in_cell == nodes_per_tile and _between(qubits, qubits) == 32  # BRAD NO HARDCODED 32
+
+        # List of 'embeddings'
+        self.embeddings = []
+
+        # For each possible chimera cell check if the next few cells are complete
+        for ii in range(size_m + 1 - sub_size_m):
+            for jj in range(size_n + 1 - sub_size_n):
+
+                # Check if the sub cells are matched
+                match = all(cells[ii + _i][jj + _j] for _i in range(sub_size_m) for _j in range(sub_size_n))
+
+                # Check if there are connections between the cells.
+                for _i in range(sub_size_m):
+                    for _j in range(sub_size_n):
+                        if sub_size_m > 1 and _i < sub_size_m - 1:
+                            match &= _between(_cell_qubits(ii + _i, jj + _j),
+                                              _cell_qubits(ii + _i + 1, jj + _j)) == size_t
+                        if sub_size_n > 1 and _j < sub_size_n - 1:
+                            match &= _between(_cell_qubits(ii + _i, jj + _j),
+                                              _cell_qubits(ii + _i, jj + _j + 1)) == size_t
+
+                if match:
+                    # Pull those cells out into an embedding.
+                    embedding = []  # BRAD REFORMAT AS PER ADTT CONVENTION
+                    for _i in range(sub_size_m):
+                        for _j in range(sub_size_n):
+                            cells[ii + _i][jj + _j] = False
+                            for uu in range(2):
+                                for kk in range(size_t):
+                                    # BRAD FLATTEN THIS OUT
+                                    embedding.append(c2i((ii + _i, jj + _j, uu, kk)))
+
+                    self.embeddings.append(embedding)
 
     @dimod.decorators.ising(1, 2)
     def sample_ising(self, h, J, **kwargs):
