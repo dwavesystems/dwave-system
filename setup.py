db3963e64bb57a774d752804e9882ac337ed39bb
Oren Shklarsky <oshklarsky@dwavesys.com>
2018-02-27 16:59:13 -0800

    add setup files

---
+++ setup.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+
+import sys
+import os
+from setuptools import setup
+
+# add __version__, __author__, __authoremail__, __description__ to this namespace
+_PY2 = sys.version_info.major == 2
+
+# change directories so this works when called from other locations. Useful in build systems.
+setup_folder_loc = os.path.dirname(os.path.abspath(__file__))
+os.chdir(setup_folder_loc)
+
+if _PY2:
+    execfile(os.path.join(".", "dwave", "system", "package_info.py"))
+else:
+    exec(open(os.path.join(".", "dwave", "system", "package_info.py")).read())
+
+install_requires = ['dimod==1.0.0.dev7']
+
+extras_require = {}
+
+packages = ['dwave.system', 'dwave.system.samplers', 'dwave.system.composites', 'dwave.system.cache']
+
+setup(
+    name='dwave_system',
+    version=__version__,
+    author=__author__,
+    author_email=__authoremail__,
+    description=__description__,
+    url='https://github.com/dwavesystems/dwave_system',
+    license='Apache 2.0',
+    packages=packages,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    namespace_packages=['dwave']
+)
