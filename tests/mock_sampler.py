655e06167b3250e07ee2700bfd7354d1dde05d9f
Alexander Condello <acondello@dwavesys.com>
2018-02-28 10:15:31 -0800

    Update mock_sampler for dimod 0.6.0

--- tests/mock_sampler.py
+++ tests/mock_sampler.py
@@ -3,38 +3,42 @@ import collections
 import dimod
 import dwave_networkx as dnx
 
+C4 = dnx.chimera_graph(4, 4, 4)
 
-class MockSampler(dimod.Sampler):
+
+class MockSampler(dimod.Sampler, dimod.Structured):
     """Create a mock sampler that can be used for tests."""
-    def __init__(self, ):
-        dimod.Sampler.__init__(self)
 
-        # set up a sampler graph and sampler's structure
-        G = dnx.chimera_graph(4, 4, 4)
-        self.structure = (sorted(G), sorted(sorted(edge) for edge in G.edges), {v: set(G[v]) for v in G})
+    nodelist = None
+    edgelist = None
+    properties = None
+    parameters = None
 
-        self.flux_biases_flag = False
+    def __init__(self):
+        self.nodelist = sorted(C4.nodes)
+        self.edgelist = sorted(sorted(edge) for edge in C4.edges)
 
-        Solver = collections.namedtuple('Solver', ['properties', 'parameters'])
-        self.solver = Solver({'chip_id': 'mock_solver', 'j_range': [-2, 1]}, {})
+        # mark the sample kwargs
+        self.parameters = parameters = {}
+        parameters['num_reads'] = ['num_reads_range']
+        parameters['flux_biases'] = ['j_range']
 
-        self.properties = self.solver.properties
+        # add the interesting properties manually
+        self.properties = properties = {}
+        properties['j_range'] = [-2.0, 1.0]
+        properties['h_range'] = [-2.0, 2.0]
+        properties['num_reads_range'] = [1, 10000]
 
-    def sample_ising(self, h, J, num_reads=10, x_flux_bias=[]):
-        # NB: need to change x_flux_bias later
+    @dimod.bqm_structured
+    def sample(self, bqm, num_reads=10, flux_biases=[]):
+        # we are altering the bqm
+        new_bqm = bqm.copy()
 
-        new_h = h.copy()
-        for v, fbo in x_flux_bias:
-            self.flux_biases_flag = True
-            if v in new_h:
-                new_h[v] += 1000 * fbo
-            else:
-                new_h[v] = 1000 * fbo
+        for v, fbo in flux_biases:
+            new_bqm.add_variable(v, 1000. * fbo)  # add the bias
 
-        response = dimod.SimulatedAnnealingSampler().sample_ising(new_h, J, num_reads=num_reads)
+        response = dimod.SimulatedAnnealingSampler().sample(new_bqm, num_reads=num_reads)
 
-        new_response = dimod.Response(dimod.SPIN)
-        for sample, in response.data(['sample']):
-            new_response.add_sample(sample, dimod.ising_energy(sample, h, J))
+        energies = [bqm.energy(sample) for sample in response.samples(sorted_by=None)]
 
-        return new_response
+        return dimod.Response.from_dicts(response.samples(sorted_by=None), {'energy': energies})
