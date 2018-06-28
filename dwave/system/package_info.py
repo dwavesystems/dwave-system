e9058c16676e35015df8a46938c5175024024717
Alexander Condello <acondello@dwavesys.com>
2018-06-28 11:14:33 -0700

    Update version to 0.4.0

--- dwave/system/package_info.py
+++ dwave/system/package_info.py
@@ -1,6 +1,6 @@
 __all__ = ['__version__', '__author__', '__authoremail__', '__description__']
 
-__version__ = '0.3.2'
+__version__ = '0.4.0'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'All things D-Wave System.'
