b60c31783cd7167e4d1b6f5075e851ffac4c04de
Alexander Condello <acondello@dwavesys.com>
2018-06-28 12:00:05 -0700

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     author=__author__,
     author_email=__authoremail__,
     description=__description__,
+    long_description=open('README.rst').read(),
     url='https://github.com/dwavesystems/dwave-system',
     license='Apache 2.0',
     packages=packages,
