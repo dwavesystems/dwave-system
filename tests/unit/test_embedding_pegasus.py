0bc6f4457288cda3eb4d33902e572b701f47862d
Melody Wong <mwong@dwavesys.com>
2018-12-20 11:20:05 -0800

    Adding tests to verify the different types that the k parameter supports

--- tests/unit/test_embedding_pegasus.py
+++ tests/unit/test_embedding_pegasus.py
@@ -6,11 +6,42 @@ import unittest
 
 
 class TestFindClique(unittest.TestCase):
-    def test_zero_clique(self):
-        k = 0
+    def test_k_parameter_int(self):
+        k_int = 5
+        m = 3
 
         # Find embedding
-        pg = pegasus_graph(3)
+        pg = pegasus_graph(m, coordinates=True)
+        embedding = find_clique_embedding(k_int, target_graph=pg)
+
+        self.assertTrue(is_valid_embedding(embedding, nx.complete_graph(k_int), pg))
+
+    def test_k_parameter_list(self):
+        k_nodes = ['one', 'two', 'three']
+        m = 4
+
+        # Find embedding
+        pg = pegasus_graph(m, coordinates=True)
+        embedding = find_clique_embedding(k_nodes, target_graph=pg)
+
+        self.assertTrue(is_valid_embedding(embedding, nx.complete_graph(k_nodes), pg))
+
+    def test_k_parameter_graph(self):
+        k_graph = nx.complete_graph(10)
+        m = 4
+
+        # Find embedding
+        pg = pegasus_graph(m, coordinates=True)
+        embedding = find_clique_embedding(k_graph, target_graph=pg)
+
+        self.assertTrue(is_valid_embedding(embedding, nx.complete_graph(k_graph), pg))
+
+    def test_zero_clique(self):
+        k = 0
+        m = 3
+
+        # Find embedding
+        pg = pegasus_graph(m)
         embedding = find_clique_embedding(k, target_graph=pg)
 
         self.assertEqual(k, len(embedding))
@@ -26,21 +57,15 @@ class TestFindClique(unittest.TestCase):
 
         self.assertTrue(is_valid_embedding(embedding, nx.complete_graph(k), pg))
 
-    def test_valid_clique_int(self):
-        k = nx.complete_graph(55)
-        m = 6
-        pg = pegasus_graph(m)
-        embedding = find_clique_embedding(k, target_graph=pg)
-        converter = pegasus_coordinates(m)
-        embedding_indices = {key: list(converter.ints(values)) for key, values in embedding.items()}
-        self.assertTrue(is_valid_embedding(embedding_indices, nx.complete_graph(k), pg))
-
     def test_valid_clique_coord(self):
         k = nx.complete_graph(55)
         m = 6
+
+        # Find embedding
         pg = pegasus_graph(m, coordinates=True)
-        emb = find_clique_embedding(k, target_graph=pg)
-        self.assertTrue(is_valid_embedding(emb, nx.complete_graph(k), pg))
+        embedding = find_clique_embedding(k, target_graph=pg)
+
+        self.assertTrue(is_valid_embedding(embedding, nx.complete_graph(k), pg))
 
     def test_impossible_clique(self):
         k = 55
