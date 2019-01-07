77cc8986fb49ccdd49be9c2aa2a48255ce3c5ed9
Alexander Condello <arcondello@gmail.com>
2019-01-07 09:50:15 -0800

    Use dimod 0.8.2
    0.8.0 and 0.8.1 contained a bug in BinaryQuadraticModel.to_ising
    that caused the EmbeddingComposite to fail.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ else:
     exec(open(os.path.join(".", "dwave", "system", "package_info.py")).read())
 
 
-install_requires = ['dimod>=0.8.1,<0.9.0',
+install_requires = ['dimod>=0.8.2,<0.9.0',
                     'dwave-cloud-client>=0.5.0,<0.6.0',
                     'dwave-networkx>=0.6.8,<0.7.0',
                     'networkx>=2.0,<3.0',
