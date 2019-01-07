d19e7f1410231d45fff1c76ad6b67db2a4ca7ed5
Alexander Condello <arcondello@gmail.com>
2019-01-07 10:54:12 -0800

    Update version 0.6.0 -> 0.7.0
    Changes
    -------
    * Use the dwave.embedding namespace for embedding utilities rather
    than dimod.embedding
    * Use dimod 0.8.x, especially `SampleSet` rather than `Response`.
    
    New Features
    ------------
    * dwave.embedding namespace
    * `broken_chains` function to find broken chains in samplesets
    * chain break resolution functions: `discard`, `majority_vote`,
    `weighted_random`, `MinimizeEnergy`
    * `embed_bqm`, `embed_ising` and `embed_qubo` functions.
    * `unembed_sampleset` function
    * `target_to_source` function that determines the source graph
    from a target graph and embedding
    * `chain_to_quadratic` function that finds the quadratic biases
    that induce a chain
    * `chain_break_frequency` function to find the frequency of chains
    being broken in a sample set
    * `find_clique_embedding` function for pegasus
    * Exceptions for specific embedding issues in
    `dwave.embedding.exceptions`
    
    Bug Fixes
    ---------
    * `EmbeddingComposite` embeds in spin-space, eliminating some energy
    range errors.

--- dwave/system/package_info.py
+++ dwave/system/package_info.py
@@ -15,7 +15,7 @@
 # ================================================================================================
 __all__ = ['__version__', '__author__', '__authoremail__', '__description__']
 
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'All things D-Wave System.'
