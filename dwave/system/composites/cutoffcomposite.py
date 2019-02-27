6a5bac7aa4e75ce54cad5b18f01bf304b01e6ed2
Alexander Condello <arcondello@gmail.com>
2019-02-27 14:52:45 -0800

    Refactor CutOffComposite

--- dwave/system/composites/cutoffcomposite.py
+++ dwave/system/composites/cutoffcomposite.py
@@ -12,24 +12,22 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
-# ================================================================================================
+# =============================================================================
 """
 A composite that removes all variables with bias smaller than the provided
 cutoff
 
 """
+import operator
 
-from heapq import heapify, heappop
-import warnings
 import numpy as np
-from dimod import BinaryQuadraticModel, SampleSet
-from dimod.core.composite import ComposedSampler
-from dimod.higherorder import poly_energies
-from dimod.vartypes import SPIN
+
+import dimod
 
 __all__ = ['CutOffComposite']
 
-class CutOffComposite(ComposedSampler):
+
+class CutOffComposite(dimod.ComposedSampler):
     """Composite to cut off variables of a problem
 
     Inherits from :class:`dimod.ComposedSampler`.
@@ -39,6 +37,15 @@ class CutOffComposite(ComposedSampler):
     Args:
        sampler (:obj:`dimod.Sampler`):
             A dimod sampler
+
+        cutoff (number, optional, default=-1):
+            The lower bound for interaction bias magnitudes. Interactions
+            with biases less than cutoff are removed. Isolated variables
+            are also not sent to the child sampler.
+
+        cutoff_vartype
+
+        comparison
 
     Examples:
        This example uses :class:`.CutOffComposite` to instantiate a
@@ -52,8 +59,13 @@ class CutOffComposite(ComposedSampler):
 
     """
 
-    def __init__(self, child_sampler):
+    @dimod.decorators.vartype_argument('cutoff_vartype')
+    def __init__(self, child_sampler, cutoff, cutoff_vartype=dimod.SPIN,
+                 comparison=operator.lt):
         self._children = [child_sampler]
+        self._cutoff = cutoff
+        self._cutoff_vartype = cutoff_vartype
+        self._comparison = comparison
 
     @property
     def children(self):
@@ -61,25 +73,19 @@ class CutOffComposite(ComposedSampler):
 
     @property
     def parameters(self):
-        param = self.child.parameters.copy()
-        param.update({'cutoff': []})
-        return param
+        return self.child.parameters.copy()
 
     @property
     def properties(self):
         return {'child_properties': self.child.properties.copy()}
 
-    def sample(self, bqm, cutoff=None, **parameters):
-        """ Cutoff and sample from the provided binary quadratic model.
+    def sample(self, bqm, **parameters):
+        """Cutoff and sample from the provided binary quadratic model.
 
         Args:
             bqm (:obj:`dimod.BinaryQuadraticModel`):
                 Binary quadratic model to be sampled from.
 
-            cutoff (float):
-                the lower bound for variable magnitudes.Variables smaller than
-                cutoff are removed before sending the problem to child sampler.
-
             **parameters:
                 Parameters for the sampling method, specified by the child sampler.
 
@@ -87,313 +93,81 @@ class CutOffComposite(ComposedSampler):
             :obj:`dimod.SampleSet`
 
         """
-
         child = self.child
-        if cutoff is None:
-            return child.sample(bqm, **parameters)
+        cutoff = self._cutoff
+        cutoff_vartype = self._cutoff_vartype
+        comp = self._comparison
 
-        bqm_new, removed = cutoff_bqm(bqm, cutoff)
-        response = child.sample(bqm_new, **parameters)
-        return _resolve_unknown(bqm, response, removed)
-
-    def sample_ising(self, h, J, offset=0, cutoff=None, **parameters):
-        """ cutoff and sample from the problem provided by h, J, offset
-
-        Args:
-            h (dict): linear biases
-
-            J (dict): quadratic or higher order biases
-
-            offset (float, optional): constant energy offset
-
-            cutoff (float):
-                the lower bound for variable magnitudes.Variables smaller than
-                cutoff are removed before sending the problem to child sampler.
-
-            **parameters:
-                Parameters for the sampling method, specified by the child sampler.
-
-        Returns:
-            :obj:`dimod.SampleSet`
-
-        """
-
-        child = self.child
-
-        # if quadratic, create a bqm and send to sample
-        if max(map(len, J.keys())) == 2:
-            warnings.warn("Higher order problem support is currently"
-                          "experimental and use case may"
-                          "change in the future", DeprecationWarning)
-
-            bqm = BinaryQuadraticModel.from_ising(h, J, offset=offset)
-            return self.sample(bqm, cutoff=cutoff, **parameters)
-
-        if cutoff is None:
-            return child.sample_ising(h, J, offset=offset, **parameters)
-
-        h_new, j_new, removed = cutoff_ising(h, J, cutoff)
-        response = child.sample_ising(h_new, j_new, offset=offset,
-                                      **parameters)
-
-        return _resolve_unknown_hubo(h, J, response, removed, offset=offset)
-
-
-def cutoff_ising(h, j, cutoff):
-    """Function to cutoff a problem defined by h,j.
-    Can handle HUBOs.
-
-    Args:
-        h (dict): linear biases
-
-        j (dict): quadratic or higher order biases
-
-        cutoff (float): the lower bound for variable magnitudes
-
-    Returns:
-        h_new (dict): the cut off linear variables
-        j_new (dict): the cut off quadratic variables
-        list: removed variables
-    """
-
-    h_new = {}
-    j_new = {}
-    removed_nodes = []
-    all_variables = set(h).union(*j)
-
-    for k, v in h.items():
-        if np.abs(v) < np.abs(cutoff):
-            removed_nodes.append(k)
+        if cutoff_vartype is dimod.SPIN:
+            original = bqm.spin
         else:
-            h_new[k] = v
-    accounted_for = set(h.keys())
+            original = bqm.binary
 
-    for k, v in j.items():
-        if np.abs(v) < np.abs(cutoff):
+        # remove all of the interactions less than cutoff
+        new = type(bqm)(original.linear,
+                        ((u, v, bias)
+                         for (u, v), bias in original.quadratic.items()
+                         if not comp(abs(bias), cutoff)),
+                        original.offset,
+                        original.vartype)
+
+        # next we check for isolated qubits and remove them, we could do this as
+        # part of the construction but the assumption is there should not be
+        # a large number in the 'typical' case
+        isolated = [v for v in new if not new.adj[v]]
+        new.remove_variables_from(isolated)
+
+        if isolated and len(new) == 0:
+            # in this case all variables are isolated, so we just put one back
+            # to serve as the basis
+            v = isolated.pop()
+            new.linear[v] = original.linear[v]
+
+        # get the samples from the child sampler and put them into the original vartype
+        sampleset = child.sample(new, **parameters).change_vartype(bqm.vartype, inplace=True)
+
+        # we now need to add the isolated back in, in a way that minimizes
+        # the energy. There are lots of ways to do this but for now we'll just
+        # do one
+        if isolated:
+            samples, variables = _restore_isolated(sampleset, bqm, isolated)
+        else:
+            samples = sampleset.record.sample
+            variables = sampleset.variables
+
+        vectors = sampleset.data_vectors
+        vectors.pop('energy')  # we're going to recalculate the energy anyway
+
+        return dimod.SampleSet.from_samples_bqm((samples, variables), bqm, **vectors)
+
+
+def _restore_isolated(sampleset, bqm, isolated):
+
+    low = -1 if bqm.vartype is dimod.SPIN else 0
+
+    samples = sampleset.record.sample
+    variables = sampleset.variables
+
+    new_samples = np.empty((len(sampleset), len(isolated)), dtype=samples.dtype)
+
+    # we don't let the isolated variables interact with eachother for now because
+    # it will slow this down substantially
+    for col, v in enumerate(isolated):
+        try:
+            neighbours, biases = zip(*((u, bias) for u, bias in bqm.adj[v].items()
+                                       if u in variables))  # ignore other isolates
+        except ValueError:
+            # happens when only neightbours are other isolated variables
+            new_samples[:, col] = bqm.linear[v] <= 0
             continue
 
-        # if a qubit was removed in h but exists in J,
-        # it cannot be set to a default alignment.
-        common = set(k) & set(removed_nodes)
-        for c in common:
-            removed_nodes.remove(c)
-        j_new[k] = v
-        accounted_for = accounted_for.union(*k)
+        idxs = [variables.index[u] for u in neighbours]
 
-    missing = all_variables.difference(accounted_for)
-    removed_nodes = set(removed_nodes).union(*missing)
+        # figure out which value for v would minimize the energy
+        # v(h_v + \sum_u J_uv * u)
+        new_samples[:, col] = samples[:, idxs].dot(biases) < -bqm.linear[v]
 
-    return h_new, j_new, list(removed_nodes)
+    if bqm.vartype is dimod.SPIN:
+        new_samples = 2*new_samples - 1
 
-
-def cutoff_bqm(bqm, cutoff):
-    """
-    function to cutoff a bqm.
-
-    Args:
-        bqm: (:obj:`dimod.BinaryQuadraticModel`):
-                Binary quadratic model to cutoff
-        cutoff (float): the lower bound for variable magnitudes
-
-    Returns:
-        cut_bqm (:obj:`dimod.BinaryQuadraticModel`): the cut off bqm
-        list: removed variables
-
-    """
-
-    h, j, removed_nodes = cutoff_ising(bqm.linear, bqm.quadratic, cutoff)
-    return BinaryQuadraticModel.from_ising(h, j, bqm.offset), removed_nodes
-
-
-def _resolve_unknown(bqm, response, removed):
-    """Helper function to bqm resolution. Keeps track of labels"""
-
-    samples, variables = _add_zero_removed(response, removed)
-    relabeling = {v: idx for idx, v in enumerate(variables)}
-    bqm.relabel_variables(relabeling, inplace=True)
-    removed_nl = [relabeling[rm] for rm in removed]
-
-    new_samples = _minimize_energy(bqm.copy(), samples, removed_nl)
-    energy_vector = bqm.energies(new_samples)
-
-    # create new sampleset
-    num_samples, num_variables = np.shape(samples)
-    datatypes = [('sample', np.dtype(np.int8), (num_variables,)),
-                 ('energy', energy_vector.dtype)]
-
-    record = response.record
-    datatypes.extend((name, record[name].dtype, record[name].shape[1:])
-                     for name in record.dtype.names if
-                     name not in {'sample',
-                                  'energy'})
-
-    data = np.rec.array(np.empty(num_samples, dtype=datatypes))
-    data.sample = samples
-    data.energy = energy_vector
-    for name in record.dtype.names:
-        if name not in {'sample', 'energy'}:
-            data[name] = record[name]
-
-    response.info['cutoff_resolved'] = removed
-    sampleset = SampleSet(data, variables, response.info, response.vartype)
-    return sampleset
-
-
-def _resolve_unknown_hubo(h, j, response, removed, offset=0):
-    """Helper function to hubo resolution. Keeps track of labels"""
-
-    # pad samples and create new variable list
-    samples, variables = _add_zero_removed(response, removed)
-    relabeling = {v: idx for idx, v in enumerate(variables)}
-    removed_nl = [relabeling[rm] for rm in removed]
-    poly = _relabeled_poly(h, j, relabeling)
-
-    # find removed variables that minimize energy
-    samples = _minimize_energy_hubo(poly, samples, removed_nl)
-    energy_vector = np.add(poly_energies(samples, poly), offset)
-
-    # create new sampleset
-    num_samples, num_variables = np.shape(samples)
-    datatypes = [('sample', np.dtype(np.int8), (num_variables,)),
-                 ('energy', energy_vector.dtype)]
-
-    record = response.record
-    datatypes.extend((name, record[name].dtype, record[name].shape[1:])
-                     for name in record.dtype.names if
-                     name not in {'sample',
-                                  'energy'})
-
-    data = np.rec.array(np.empty(num_samples, dtype=datatypes))
-    data.sample = samples
-    data.energy = energy_vector
-    for name in record.dtype.names:
-        if name not in {'sample', 'energy'}:
-            data[name] = record[name]
-
-    response.info['cutoff_resolved'] = removed
-    sampleset = SampleSet(data, variables, response.info, response.vartype)
-    return sampleset
-
-
-def _add_zero_removed(response, removed):
-    """Helper function. Adds removed as zeros into samples"""
-    variables = list(response.variables) + removed
-    samples = np.asarray(response.record.sample)
-    samples = np.concatenate((samples, np.zeros((len(samples), len(removed)),
-                                                dtype=np.int8)), axis=1)
-
-    return samples, variables
-
-
-def _minimize_energy(bqm, samples, removed):
-    """Resolves the unknown variables by energy minimization"""
-
-    if bqm.vartype is SPIN:
-        ZERO = -1
-    else:
-        ZERO = 0
-
-    def _minenergy(arr):
-
-        energies = []
-        for cidx in removed:
-            en = bqm.linear[cidx] + sum(
-                arr[idx] * bqm.adj[cidx][idx] for idx in
-                bqm.adj[cidx])
-            energies.append([-abs(en), en, cidx])
-        heapify(energies)
-
-        while energies:
-            _, e, i = heappop(energies)
-
-            arr[i] = val = ZERO if e > 0 else 1
-
-            for energy_triple in energies:
-                k = energy_triple[2]
-                if k in bqm.adj[i]:
-                    energy_triple[1] += val * bqm.adj[i][k]
-                energy_triple[0] = -abs(energy_triple[1])
-
-            heapify(energies)
-
-        return arr
-
-    return np.apply_along_axis(_minenergy, 1, samples)
-
-
-def _minimize_energy_hubo(poly, samples, removed):
-    """Hubo version of minimize energy"""
-
-    adj = {}
-    for key, val in poly.items():
-        if len(key) <= 1:
-            continue
-
-        for var in key:
-            keytmp = list(key)
-            keytmp.remove(var)
-            if var in adj:
-                adj[var].update({tuple(keytmp): val})
-            else:
-                adj[var] = {tuple(keytmp): val}
-
-    def _minenergy(arr):
-
-        energies = []
-        for cidx in removed:
-            en = poly.get((cidx,), 0) + sum(np.prod([arr[i] for i in idx]) * val
-                                            for
-                                            idx, val in adj[cidx].items())
-            energies.append([-abs(en), en, cidx])
-        heapify(energies)
-
-        while energies:
-            _, e, i = heappop(energies)
-
-            arr[i] = val = -1 if e > 0 else 1
-
-            for energy_triple in energies:
-                k = energy_triple[2]
-                tmp_sum = 0
-                for jj, intval in adj[i].items():
-                    if k in jj:
-                        tmp_sum += intval * val * np.prod(arr[i] for i in jj
-                                                          if i != k)
-                energy_triple[1] += tmp_sum
-                energy_triple[0] = -abs(energy_triple[1])
-
-            heapify(energies)
-
-        return arr
-
-    return np.apply_along_axis(_minenergy, 1, samples)
-
-
-def _relabeled_poly(h, j, label_dict):
-    """ Creates relabeled polynomial dict.
-
-    given h, j and a relabeling dictionary, will create a polynomial
-    with the new labels
-
-    Args:
-        h (dict): a dict of linear variables
-
-        j (dict): a dict of quadratic and higher order variables
-
-        label_dict (dict): a dict for relabeling e.g. {old_label:new_label}
-
-    Returns:
-        dict: a higher order problem dict that contains linear,
-              quadratic and n-order terms written in a new labeling
-              scheme provided by label_dict
-
-    """
-
-    poly = {}
-    for k, v in h.items():
-        new_tup = (label_dict[k],)
-        poly[new_tup] = v
-    for k, v in j.items():
-        new_tup = tuple((label_dict[vidx] for vidx in list(k)))
-        poly[new_tup] = v
-    return poly
+    return np.concatenate((samples, new_samples), axis=1), list(variables) + isolated
