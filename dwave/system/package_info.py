e488fa98125d399d68330f2de1255aaf03e8b023
Alexander Condello <arcondello@gmail.com>
2019-05-06 09:31:23 -0700

    Revert version number increment until pre-deploy

--- dwave/system/package_info.py
+++ dwave/system/package_info.py
@@ -15,7 +15,7 @@
 # ================================================================================================
 __all__ = ['__version__', '__author__', '__authoremail__', '__description__']
 
-__version__ = '0.7.3'
+__version__ = '0.7.2'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'All things D-Wave System.'
