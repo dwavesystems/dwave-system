d4fd4432fd402c8f08235dbe1391fc5753bfc63c
Alexander Condello <arcondello@gmail.com>
2019-06-17 15:21:35 -0700

    Simplify VirtualGraphComposite QPU tests

--- tests/qpu/test_virtual_graph_composite.py
+++ tests/qpu/test_virtual_graph_composite.py
@@ -12,7 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
-# ================================================================================================
+# =============================================================================
 import itertools
 import unittest
 
@@ -24,17 +24,27 @@ from dwave.cloud.exceptions import ConfigFileError
 from dwave.system.composites import VirtualGraphComposite
 from dwave.system.samplers import DWaveSampler
 
-try:
-    DWaveSampler()
-    _config_found = True
-except (ValueError, ConfigFileError):
-    _config_found = False
 
-
-@unittest.skipUnless(_config_found, "no configuration found to connect to a system")
 class TestVirtualGraphComposite(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        try:
+            cls.qpu = DWaveSampler(solver=dict(qpu=True))
+        except (ValueError, ConfigFileError):
+            cls.qpu = False
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.qpu:
+            cls.qpu.client.close()
+
+    def setUp(self):
+        if not self.qpu:
+            self.skipTest("no qpu available")
+
     def test_construction(self):
-        child_sampler = DWaveSampler()
+        child_sampler = self.qpu
 
         # get an embedding
         K10_edges = list(itertools.combinations(range(10), 2))
@@ -46,24 +56,9 @@ class TestVirtualGraphComposite(unittest.TestCase):
 
         h = {}
         J = {edge: -1 for edge in K10_edges}
-        sampler.sample_ising(h, J)
 
-        sampler.sample_ising(h, J, apply_flux_bias_offsets=False)
+        # run with fbo
+        sampler.sample_ising(h, J).resolve()
 
-    def test_reverse_annealing(self):
-        child_sampler = DWaveSampler()
-
-        # get an embedding
-        K10_edges = list(itertools.combinations(range(10), 2))
-        embedding = minorminer.find_embedding(K10_edges, child_sampler.edgelist)
-
-        sampler = VirtualGraphComposite(child_sampler, embedding)
-
-        h = {}
-        J = {edge: -1 for edge in K10_edges}
-
-        kwargs = {'initial_state': {v: 0 for v in set().union(*J)},
-                  'anneal_schedule': [(0, 1), (55.0, 0.45), (155.0, 0.45), (210.0, 1)]}
-
-        # sample and resolve
-        sampler.sample_ising(h, J, **kwargs).samples()
+        # and again without
+        sampler.sample_ising(h, J, apply_flux_bias_offsets=False).resolve()
