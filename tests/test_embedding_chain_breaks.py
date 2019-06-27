fe9c4c46e0a62e5acfe3bd389f753f5f8df9846b
Alexander Condello <arcondello@gmail.com>
2019-06-27 15:44:43 -0700

    Add unittests for chain break method API

--- tests/test_embedding_chain_breaks.py
+++ tests/test_embedding_chain_breaks.py
@@ -15,9 +15,12 @@
 # =============================================================================
 import unittest
 
-import numpy as np
+from contextlib import contextmanager
+
 
 import dimod
+import numpy as np
+import six
 
 import dwave.embedding
 
@@ -59,7 +62,45 @@ class TestBrokenChains(unittest.TestCase):
         broken = dwave.embedding.broken_chains(samples_matrix, chain_list)
 
 
-class TestDiscard(unittest.TestCase):
+class TestChainBreakResolutionAPI():
+    chains = [[0, 1], [2, 4], [3]]  # needs to be available to MinimizeEnergy
+
+    def subTest(self, *args, **kwargs):
+        # python2 does not support subTest so we just use a null context manager
+        if six.PY2:
+            @contextmanager
+            def nullcontext(enter_result=None):
+                yield enter_result
+            return nullcontext()
+        return super(TestChainBreakResolutionAPI, self).subTest(*args, **kwargs)
+
+    def test_api(self):
+        cases = {}
+        cases['ndarray_binary'] = np.triu(np.ones((5, 5)))
+        cases['ndarray_spin'] = 2*np.triu(np.ones((5, 5))) - 1
+        cases['listoflists_binary'] = [[1, 1, 1, 1, 1],
+                                       [0, 1, 1, 1, 1],
+                                       [0, 0, 1, 1, 1],
+                                       [0, 0, 0, 1, 1],
+                                       [0, 0, 0, 0, 1]]
+
+        for description, samples in cases.items():
+            with self.subTest(description):
+                resolved, idxs = self.chain_break_method(samples, self.chains)
+
+                # correct number of variables (1 per chain)
+                self.assertEqual(resolved.shape[1], len(self.chains))
+
+                # correct number of rows
+                self.assertEqual(resolved.shape[0], len(idxs))
+
+
+class TestDiscard(TestChainBreakResolutionAPI, unittest.TestCase, ):
+
+    # for the API tests
+    def setUp(self):
+        self.chain_break_method = dwave.embedding.discard
+
     def test_discard_no_breaks_all_ones_identity_embedding(self):
 
         samples_matrix = np.array(np.ones((100, 50)), dtype='int8')
@@ -100,7 +141,12 @@ class TestDiscard(unittest.TestCase):
         np.testing.assert_array_equal(idx, [0])
 
 
-class TestMajorityVote(unittest.TestCase):
+class TestMajorityVote(TestChainBreakResolutionAPI, unittest.TestCase):
+
+    # for the API tests
+    def setUp(self):
+        self.chain_break_method = dwave.embedding.majority_vote
+
     def test_typical_spin(self):
         S = np.array([[-1, +1, -1, +1],
                       [+1, +1, -1, +1],
@@ -150,7 +196,13 @@ class TestMajorityVote(unittest.TestCase):
         self.assertEqual(set().union(*samples), {-1, 1})  # should be spin-valued
 
 
-class TestMinimizeEnergy(unittest.TestCase):
+class TestMinimizeEnergy(TestChainBreakResolutionAPI, unittest.TestCase):
+    # for the API tests
+    def setUp(self):
+        embedding = dict(zip('abcdefghijk', self.chains))
+        bqm = dimod.BinaryQuadraticModel.from_ising({v: 0 for v in embedding}, {})
+        self.chain_break_method = dwave.embedding.MinimizeEnergy(bqm, embedding)
+
     def test_minimize_energy(self):
         embedding = {0: (0, 5), 1: (1, 6), 2: (2, 7), 3: (3, 8), 4: (4, 10)}
         h = []
