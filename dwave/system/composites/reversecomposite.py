27e2b0b3b131cceead1d8e481165c6a47b219114
Isil Ozfidan <iozfidan@dwavesys.com>
2019-11-13 14:15:22 -0800

    bug fix, all variable handling

--- dwave/system/composites/reversecomposite.py
+++ dwave/system/composites/reversecomposite.py
@@ -49,7 +49,7 @@ class ReverseAdvanceComposite(dimod.ComposedSampler):
     def properties(self):
         return {'child_properties': self.child.properties.copy()}
 
-    def sample(self, bqm, schedules=None, **parameters):
+    def sample(self, bqm, anneal_schedules=None, **parameters):
         """ Composite that advances a sample using reverse annealing along a
         given set of anneal schedules. Always selects the most probable lowest energy sample out of
         the previous steps return
@@ -71,7 +71,7 @@ class ReverseAdvanceComposite(dimod.ComposedSampler):
         """
         child = self.child
 
-        if schedules is None:
+        if anneal_schedules is None:
             return child.sample(bqm, **parameters)
 
         vartype_values = list(bqm.vartype.value)
@@ -80,52 +80,38 @@ class ReverseAdvanceComposite(dimod.ComposedSampler):
         else:
             initial_state = parameters.pop('initial_state')
 
+        if not isinstance(initial_state,dict):
+            raise TypeError("initial state provided must be a dict, received {}".format(initial_state))
+
         if 'reinitialize_state' not in parameters:
             parameters['reinitialize_state'] = True
+            parameters['answer_mode'] = 'histogram'
 
-        # there is gonna be way too much data generated - better to histogram them
-        parameters['answer_mode'] = 'histogram'
-
-        # prepare data fields for the new sampleset object
-        samples = []
-        energy = []
-        num_occurrences = []
-        initial_states = []
-        schedule_idxs = []
-        sample, variables = dimod.as_samples(initial_state)
-        datatypes = [('sample', sample.dtype, (len(variables),)),
-                     ('energy', np.float16),
-                     ('num_occurrences', np.int8),
-                     ('initial_state', sample.dtype, (len(variables),)),
-                     ('schedule_index', np.int8)]
-
-        for schedule_idx, anneal_schedule in enumerate(schedules):
-            sampleset = child.sample(bqm, anneal_schedule=anneal_schedule, initial_state=initial_state,
+        vectors = {}
+        for schedule_idx, anneal_schedule in enumerate(anneal_schedules):
+            sampleset = child.sample(bqm, anneal_schedule=anneal_schedule,initial_state = initial_state,
                                      **parameters)
 
-            # collect data from sampleset object
-            samples = [*samples, *sampleset.record.sample]
-            energy = [*energy, *sampleset.record.energy]
-            num_occurrences = [*num_occurrences, *sampleset.record.num_occurrences]
+            initial_state,_ = dimod.as_samples(initial_state)
+            vectors = _update_data_vector(vectors,sampleset,
+                                           {'initial_state':[initial_state[0]]*len(sampleset.record.energy),
+                                            'schedule_index':[schedule_idx]*len(sampleset.record.energy)})
 
-            initial_state, _ = dimod.as_samples(initial_state)
-            initial_states = [*initial_states, *[initial_state] * len(sampleset.record.energy)]
-            schedule_idxs = [*schedule_idxs, *[schedule_idx] * len(sampleset.record.energy)]
+            if parameters['reinitialize_state']:
+                # if reinitialize is on, choose the lowest energy, most probable state for next iteration
+                gse = sampleset.first.energy
+                b = sampleset.record[sampleset.record.energy == gse]
+                b.sort(order='num_occurrences')
+                initial_state = dict(zip(sampleset.variables, b[-1].sample))
+            else:
+                # if not reinitialized, take the last state as the next initial state
+                initial_state = dict(zip(sampleset.variables, sampleset.record.sample[-1]))
 
-            # initialize the new initial_state as the lowest energy, most probable sample
-            gse = sampleset.first.energy
-            b = sampleset.record[sampleset.record.energy == gse]
-            b.sort(order='num_occurrences')
-            initial_state = dict(zip(sampleset.variables, b[-1].sample))
-
-        record = np.rec.array(np.zeros(len(energy), dtype=datatypes))
-        record['sample'] = samples
-        record['energy'] = energy
-        record['num_occurrences'] = num_occurrences
-        record['schedule_index'] = schedule_idxs
-        record['initial_state'] = initial_states
-
-        return dimod.SampleSet(record, variables, {'schedules': schedules}, bqm.vartype)
+        samples = vectors.pop('sample')
+        return dimod.SampleSet.from_samples((samples, bqm.variables),
+                                bqm.vartype,
+                                info={'anneal_schedules': anneal_schedules},
+                                **vectors)
 
 
 class BatchReverseComposite(dimod.ComposedSampler):
@@ -180,34 +166,39 @@ class BatchReverseComposite(dimod.ComposedSampler):
         parameters['answer_mode'] = 'histogram'
 
         # prepare data fields for the new sampleset object
-        samples = []
-        energy = []
-        num_occurrences = []
-        initial_states_array = []
 
-        datatypes = [('sample', np.int8, (len(bqm),)),
-                     ('energy', np.float16),
-                     ('num_occurrences', np.int8),
-                     ('initial_state', np.int8, (len(bqm),))]
-
+        vectors = {}
         for initial_state in initial_states:
+
             if not isinstance(initial_state, dict):
                 initial_state = dict(zip(bqm.variables, initial_state))
 
             sampleset = child.sample(bqm, initial_state=initial_state, **parameters)
+            initial_state_, _ = dimod.as_samples(initial_state)
+            vectors = _update_data_vector(vectors, sampleset,
+                                          {'initial_state': [initial_state_[0]] * len(sampleset.record.energy)})
 
-            # collect data from sampleset object
-            samples = [*samples, *sampleset.record.sample]
-            energy = [*energy, *sampleset.record.energy]
-            num_occurrences = [*num_occurrences, *sampleset.record.num_occurrences]
+        samples = vectors.pop('sample')
 
-            initial_state, _ = dimod.as_samples(initial_state)
-            initial_states_array = [*initial_states_array, *[initial_state] * len(sampleset.record.energy)]
+        return dimod.SampleSet.from_samples((samples, bqm.variables),
+                                bqm.vartype,
+                                info={},
+                                **vectors)
 
-        record = np.rec.array(np.zeros(len(energy), dtype=datatypes))
-        record['sample'] = samples
-        record['energy'] = energy
-        record['num_occurrences'] = num_occurrences
-        record['initial_state'] = initial_states_array
 
-        return dimod.SampleSet(record, bqm.variables, {}, bqm.vartype)
+def _update_data_vector(vectors, sampleset,additional_parameters=None):
+
+    var_names = sampleset.record.dtype.names
+    for name in var_names:
+        try:
+            vectors[name] = [*vectors[name], *sampleset.record[name]]
+        except KeyError:
+            vectors[name] = sampleset.record[name]
+
+    for key,val in additional_parameters.items():
+        if key not in var_names:
+            try:
+                vectors[key] = [*vectors[key], *val]
+            except KeyError:
+                vectors[key] = val
+    return vectors
