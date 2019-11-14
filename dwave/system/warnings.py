69092e34ca386a9b2fefcf274a484a0413a6e3ce
Alexander Condello <arcondello@gmail.com>
2019-11-13 17:47:41 -0800

    Support type, level and data in prototype warning generation

--- dwave/system/warnings.py
+++ dwave/system/warnings.py
@@ -13,12 +13,15 @@
 #    limitations under the License.
 #
 # =============================================================================
-from enum import Enum
+import enum
+import logging
 
 import six
 
+from dwave.embedding import broken_chains
 
-class WarningAction(Enum):
+
+class WarningAction(enum.Enum):
     IGNORE = 'ignore'
     SAVE = 'save'
 
@@ -31,6 +34,14 @@ IGNORE = WarningAction.IGNORE
 SAVE = WarningAction.SAVE
 # LOG = WarningAction.LOG
 # RAISE = WarningAction.RAISE
+
+
+class ChainBreakWarning(UserWarning):
+    pass
+
+
+class ChainLengthWarning(UserWarning):
+    pass
 
 
 class WarningHandler(object):
@@ -49,32 +60,91 @@ class WarningHandler(object):
         self.action = action
 
     # todo: let user override __init__ parameters with kwargs
-    def issue(self, msg, func=None):
+    def issue(self, msg, category=None, func=None, level=logging.WARNING,
+              data=None):
         """Issue a warning.
 
         Args:
-            msg (str): The warning message
+            msg (str):
+                The warning message
+
+            category (Warning):
+                The warning category class. Defaults to UserWarning.
+
+            level (int):
+                The level of warning severity. Uses the logging warning levels.
+
             func (function):
                 A function that is executed in the case that the warning level
-                is not IGNORE. The warning is issued only if the function
-                returns True. This is used to lazily execute expensive tests.
+                is not IGNORE. The function should return a 2-tuple containing
+                a bool specifying whether the warning should be saved/raised
+                and any relevent data associated with the warning as a
+                dictionary/None. This overrides anything provided in the `data`
+                kwarg.
+
+            data (dict):
+                Any data relevent to the warning.
 
         """
 
         if self.action is IGNORE:
             return
-        elif (func is not None and not func()):
-            # func condition was not met so do nothing
-            return
-        elif self.action is SAVE:
-            self.saved.append(msg)
+
+        if func is not None:
+            valid, data = func()
+            if not valid:
+                return
+
+        if category is None:
+            category = UserWarning
+
+        if data is None:
+            data = {}
+
+        if self.action is SAVE:
+            self.saved.append(dict(type=category,
+                                   message=msg,
+                                   level=level,
+                                   data=data))
         else:
             raise TypeError("unknown action")
 
-    # some hard-coded warnings for convenience
+    # some hard-coded warnings for convenience or for expensive operations
 
     def chain_length(self, embedding, length=7):
-        def func():
-            return any(len(chain) > length for chain in embedding.values())
+        if self.action is IGNORE:
+            return
 
-        self.issue('chain length greater than {}'.format(length), func)
+        for v, chain in embedding.items():
+            if len(chain) <= length:
+                continue
+
+            self.issue("chain length greater than {}".format(length),
+                       category=ChainLengthWarning,
+                       data=dict(target_variables=chain,
+                                 source_variables=[v]),
+                       )
+
+    def chain_break(self, sampleset, embedding):
+        if self.action is IGNORE:
+            return
+
+        ground = sampleset.lowest()
+        variables = list(embedding)
+        chains = [embedding[v] for v in variables]
+        broken = broken_chains(ground, chains)
+
+        if not (len(sampleset) and broken.any()):
+            return
+
+        for nc, chain in enumerate(chains):
+            for row in range(broken.shape[0]):
+                if not broken[row, nc]:
+                    continue
+
+                self.issue("a chain is broken in the lowest energy samples found",
+                           category=ChainBreakWarning,
+                           level=logging.ERROR,
+                           data=dict(target_variables=chain,
+                                     source_variables=[variables[nc]]),
+                           )
