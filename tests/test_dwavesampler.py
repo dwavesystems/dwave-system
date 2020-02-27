8fd9e6aa15dd1aaa9b35868e5a331653547d4ec9
arcondello <arcondello@gmail.com>
2020-02-26 17:25:23 -0800

    Remove old test MockSolver code

--- tests/test_dwavesampler.py
+++ tests/test_dwavesampler.py
@@ -94,60 +94,6 @@ class MockSolver():
 
         return future
 
-    def sample_ising(self, h, J, **kwargs):
-        for key in kwargs:
-            if key not in self.properties['parameters']:
-                raise ValueError
-        result = {'num_variables': 2048,
-                  'format': 'qp',
-                  'num_occurrences': [1],
-                  'active_variables': list(range(2048)),
-                  'solutions': [[random.choice((-1, +1)) for __ in range(2048)]],
-                  'timing': {'total_real_time': 11511, 'anneal_time_per_run': 20,
-                             'post_processing_overhead_time': 2042, 'qpu_sampling_time': 164,
-                             'readout_time_per_run': 123,
-                             'qpu_delay_time_per_sample': 21,
-                             'qpu_anneal_time_per_sample': 20,
-                             'total_post_processing_time': 2042,
-                             'qpu_programming_time': 8740,
-                             'run_time_chip': 164,
-                             'qpu_access_time': 11511,
-                             'qpu_readout_time_per_sample': 123},
-                  'occurrences': [1]}
-        result['samples'] = result['solutions']
-        result['energies'] = [dimod.ising_energy(sample, h, J) for sample in result['samples']]
-        future = Future()
-        future.id = uuid4()
-        future.set_result(result)
-        return future
-
-    def sample_qubo(self, Q, **kwargs):
-        for key in kwargs:
-            if key not in self.properties['parameters']:
-                raise ValueError
-        result = {'num_variables': 2048,
-                  'format': 'qp',
-                  'num_occurrences': [1],
-                  'active_variables': list(range(2048)),
-                  'solutions': [[random.choice((0, 1)) for __ in range(2048)]],
-                  'timing': {'total_real_time': 11511, 'anneal_time_per_run': 20,
-                             'post_processing_overhead_time': 2042, 'qpu_sampling_time': 164,
-                             'readout_time_per_run': 123,
-                             'qpu_delay_time_per_sample': 21,
-                             'qpu_anneal_time_per_sample': 20,
-                             'total_post_processing_time': 2042,
-                             'qpu_programming_time': 8740,
-                             'run_time_chip': 164,
-                             'qpu_access_time': 11511,
-                             'qpu_readout_time_per_sample': 123},
-                  'occurrences': [1]}
-        result['samples'] = result['solutions']
-        result['energies'] = [dimod.qubo_energy(sample, Q) for sample in result['samples']]
-        future = Future()
-        future.id = uuid4()
-        future.set_result(result)
-        return future
-
 
 class TestDwaveSampler(unittest.TestCase):
     @mock.patch('dwave.system.samplers.dwave_sampler.Client')
