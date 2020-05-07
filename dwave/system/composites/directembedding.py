8fb834e4a5086c8e013106fe27d07739089d1e86
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2020-05-07 12:17:26 -0700

    Change from relative to absolute edges

--- dwave/system/composites/directembedding.py
+++ dwave/system/composites/directembedding.py
@@ -69,16 +69,17 @@ class DirectChimeraTilesEmbeddingComposite(dimod.ComposedSampler):
        {0: [20], 1: [25], 4: [440], 6: [450], 11: [95], 14: [451], 15: [456]}
 
     Note:
-       For a Chimera QPU especially, direct embedding is often simply done by
-       setting the desired node biases in the submission of a
+       direct embedding is useful when it's important that each node be represented
+       by a single qubit. For a Chimera QPU especially, direct embedding is often
+       simply done by setting the desired node biases in the submission of a
        :class:`~dwave.system.samplers.DWaveSampler()` for example,
 
        >>> samplset = DWaveSampler().sample_ising({}, {(0, 4): 1})  # doctest: +SKIP
 
-       You can shift embedding to another unit cell, :math: `n`, just by adding an offset of
-       :math:`8*n` to the zero node. But if you want the emebdding to be agnostic
-       to the selected QPU's working graph and topology, this composite can be
-       useful.
+       You can shift embedding to another unit cell, :math: `n`, just by adding
+       an offset of :math:`8*n` to the zero node. But if you want the emebdding
+       to be agnostic to the selected QPU's working graph and topology, this
+       composite can be useful.
     """
     def __init__(self, child_sampler):
 
@@ -132,126 +133,103 @@ class DirectChimeraTilesEmbeddingComposite(dimod.ComposedSampler):
         """
 
         if self.topology_type == "pegasus":
-            G_ideal = pegasus_graph(self.tiles)
-            G_working = dnx.pegasus_graph(self.tiles,
+            self.couplers = dnx.pegasus_graph(self.tiles,
                          node_list=self.child.nodelist,
-                         edge_list=self.child.edgelist)
-            self.couplers = G_working.edges
+                         edge_list=self.child.edgelist).edges
             embed = self._pegasus_embedding
         else:
-            G_ideal = chimera_graph(self.tiles, 1)
             self.couplers = self.child.edgelist
             embed = self._chimera_embedding
 
         max_nodes = 8*self.tiles if self.topology_type == "chimera" else \
-                    3*8*self.tiles
-        if not set(val for tup in Q.keys() for val in tup).issubset(set
-           (range(max_nodes))):
+                    3*8*self.tiles  #Rough (and extreme) limit on size
+        if not set(val for tup in Q.keys() for val in tup).issubset(
+               set(range(max_nodes))):
            msg = "Composite supports only indexed nodes"
            raise BinaryQuadraticModelStructureError(msg)
 
         uncoupled = (set(val for tup in Q.keys() for val in tup if tup[0] == tup[1]) -
-            set(val for tup in Q.keys() for val in tup if tup[0] != tup[1]))
+                     set(val for tup in Q.keys() for val in tup if tup[0] != tup[1]))
         if uncoupled:
            msg = "Composite does not support singleton nodes: {}".format(uncoupled)
            raise BinaryQuadraticModelStructureError(msg)
 
         self.num_cells = max([max(u, v) for u,v in Q.keys()]) //8 + 1
 
-        # Map chimera-indexed edges to relative vertical/horizontal positions
+        # Map to intra-cell and inter-cell couplers per unit cell
         self.edges_vh = list()
         self.edges_hh = list()
         for i in range(self.num_cells):
-            self.edges_vh.append([(min(edge) % 4, max(edge) % 4)
+            self.edges_vh.append([(min(edge), max(edge))
                  for edge in Q.keys() if min(edge) != max(edge) and
                                          min(edge) >= 8 * i and
                                          max(edge) < 8 * (i + 1)])
-            self.edges_hh.append([(edge[0] % 4, edge[1] % 4)
+            self.edges_hh.append([(edge[0], edge[1])
                  for edge in Q.keys() if min(edge) in range(8 * i, 8 * (i + 1)) and
                                          max(edge) in range(8 * (i + 1), 8 * (i + 2))])
 
-        try:
-            embedding = embed()
-        except EmbeddingError:
-            try:
-                self.couplers = G_ideal.edges
-                embedding = embed()
-            except EmbeddingError:
-                msg = "Graph must be up to {} horizontally adjacent \
-                       Chimera unit cells".format(self.tiles)
-                raise BinaryQuadraticModelStructureError(msg)
+        embedding = embed()
 
         return FixedEmbeddingComposite(self.child, embedding=embedding).sample_qubo(
-                                       Q, **parameters)
+                                           Q, **parameters)
 
     def _chimera_embedding(self):
-        """
-        Map the problem's logical edges to edges of one or more unit cells in a
-        row of a Chimera QPU's working graph.
-        """
+        """Map problem's edges to qubits in row of unit cells in a Chimera QPU."""
 
         for row in range(self.tiles):
             for column in range(self.tiles - self.num_cells):
 
                 q0 = 8*self.tiles*row + 8*column
 
-                if all((set((v + q0 + 8*i, h + q0 + 8*i + 4) for
+                if all((set((v + q0, h + q0) for
                             v, h in self.edges_vh[i])
                        |  # Exploit empty self.edges_hh set for i = self.num_cells
-                        set((v + q0 + 8*i + 4, h + q0 + 8*(i + 1) + 4) for
+                        set((v + q0, h + q0) for
                             v, h in self.edges_hh[i])
                        ).issubset(self.couplers) for i in range(self.num_cells)):
 
                        embedding = dict()
                        for i in range(self.num_cells):
-                           embedding.update({v + 8*i: [v + q0 + 8*i]
+                           embedding.update({v: [v + q0]
                                             for v, h in self.edges_vh[i]})
-                           embedding.update({h + 8*i + 4: [h + q0 + 8*i + 4]
+                           embedding.update({h: [h + q0]
                                             for v, h in self.edges_vh[i]})
-                           embedding.update({h0 + 8*i + 4: [h0 + q0 + 8*i + 4]
+                           embedding.update({h0: [h0 + q0]
                                             for h0, h1 in self.edges_hh[i]})
                            # Exploit empty self.edges_hh set for i = self.num_cells
-                           embedding.update({h1 + 8*(i + 1) + 4:
-                                            [h1 + q0 + 8*(i+1) + 4] for h0, h1
-                                            in self.edges_hh[i]})
+                           embedding.update({h1: [h1 + q0]
+                                            for h0, h1 in self.edges_hh[i]})
 
                        return (embedding)
 
         raise EmbeddingError("No embedding found")
 
     def _pegasus_embedding(self):
-        """
-        Map the problem's logical edges to indexed edges of one or more :math:`K_{4,4}`
-        unit cells in a row of a Pegaus QPU's working graph.
-        """
+        """Map problem's edges to row of :math:`K_{4,4}` cells in a Pegaus QPU."""
 
         w, z, k_44 = self._scan_cells()
 
         embedding = dict()
         for i in range(self.num_cells):
 
-            embedding.update({k + 8*i: [dnx.pegasus_coordinates(self.tiles).pegasus_to_linear(
-                       (0, w + i, k + 4*k_44, z))] for k, h in self.edges_vh[i]})
-
             nodes_h = list(dnx.pegasus_coordinates(self.tiles).iter_pegasus_to_linear(
                            self._h_pair(w + i, z, k_44)))
-            embedding.update({k + 8*i + 4: [nodes_h[k]] for v, k
-                              in self.edges_vh[i]})
-            embedding.update({h0 + 8*i + 4: [nodes_h[h0]] for
-                              h0, h1 in self.edges_hh[i]})
-            # Exploit empty self.edges_hh set for i = self.num_cells
             nodes_h1 = list(dnx.pegasus_coordinates(self.tiles).iter_pegasus_to_linear(
                             self._h_pair(w + i + 1, z, k_44)))
-            embedding.update({h1 + 8*(i + 1) + 4: [nodes_h1[h1]] for
+
+            embedding.update({v: [dnx.pegasus_coordinates(self.tiles).pegasus_to_linear(
+                       (0, w + i, v - 8*i + 4*k_44, z))] for v, h in self.edges_vh[i]})
+            embedding.update({h: [nodes_h[h - 8*i - 4]] for v, h in self.edges_vh[i]})
+            embedding.update({h0: [nodes_h[h0 - 8*i - 4]] for
+                              h0, h1 in self.edges_hh[i]})
+            # Exploit empty self.edges_hh set for i = self.num_cells
+            embedding.update({h1: [nodes_h1[h1 - 8*(i + 1) - 4]] for
                               h0, h1 in self.edges_hh[i]})
 
         return(embedding)
 
     def _scan_cells(self):
-        """
-        Find horizontally adjacent :math:`K_{4,4}` unit cells with all the required
-        internal and external couplers.
-        """
+        """Find horizontally adjacent :math:`K_{4,4}` cells with needed couplers."""
 
         for z in range(self.tiles):
             for w in range(self.tiles - self.num_cells):
@@ -259,39 +237,35 @@ class DirectChimeraTilesEmbeddingComposite(dimod.ComposedSampler):
 
                     if all(self._couplers_vh(w, z, k_44, i) and
                            self._couplers_hh(w, z, k_44, i)
-                           for i in range(self.num_cells)):
-
+                              for i in range(self.num_cells)):
                         return (w, z, k_44)
 
         raise EmbeddingError("No embedding found")
 
     def _couplers_vh(self, w, z, k_44, cell):
-        """
-        Check edges between left/vertical and right/horizontal nodes of a
-        :math:`K_{4,4}` unit cell.
-        """
+        """Check internal couplers in a :math:`K_{4,4}` unit cell."""
 
         nodes_v = list(dnx.pegasus_coordinates(self.tiles).iter_pegasus_to_linear(
                        [(0, w + cell, k, z) for k in range(4*k_44, 4*k_44 + 4)]))
         nodes_h = list(dnx.pegasus_coordinates(self.tiles).iter_pegasus_to_linear(
                        self._h_pair(w + cell, z, k_44)))
 
-        couplers_vh = [(nodes_v[v], nodes_h[h]) for v, h in self.edges_vh[cell]]
+        couplers_vh = [(nodes_v[v - 8*cell], nodes_h[h - 8*cell - 4]) for
+                       v, h in self.edges_vh[cell]]
 
         return all(edge in self.couplers for edge in couplers_vh)
 
     def _couplers_hh(self, w, z, k_44, cell):
-        """
-        Check edges between horizontal shores of two adjacent :math:`K_{4,4}` unit
-        cells.
-        """
+        """Check external couplers between horizontal shores of unit cells."""
 
         left = list(dnx.pegasus_coordinates(self.tiles).iter_pegasus_to_linear(
-               self._h_pair(w + cell, z, k_44)))
+                    self._h_pair(w + cell, z, k_44)))
         right = list(dnx.pegasus_coordinates(self.tiles).iter_pegasus_to_linear(
-               self._h_pair(w + cell + 1, z, k_44)))
+                     self._h_pair(w + cell + 1, z, k_44)))
 
-        couplers_hh = [(left[v], right[h]) for v, h in self.edges_hh[cell]]
+        couplers_hh = [(left[h0 - 8*cell - 4], right[h1 - 8*(cell + 1) - 4])
+                       for h0, h1 in self.edges_hh[cell]]
+
         return all(edge in self.couplers for edge in couplers_hh)
 
     def _h_pair(self, w, z, k_44):
@@ -299,4 +273,5 @@ class DirectChimeraTilesEmbeddingComposite(dimod.ComposedSampler):
 
         ww = z + 1 if k_44 > 0 else z
         zz = w - 1 if k_44 == 0 else w
+
         return [(1, ww, j, zz) for j in range(8 - 4*k_44, 12 - 4*k_44)]
