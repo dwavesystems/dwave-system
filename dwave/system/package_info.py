8a7bc443b77bbb6d10fcdec3f49fafd955e0232a
Alexander Condello <arcondello@gmail.com>
2020-06-23 11:19:07 -0700

    Update version 0.9.5 -> 0.9.6
    Fixes
    -----
    * `LeapHybridSampler` now correctly chooses solvers that accept the `bqm` problem type
    
    New Features
    ------------
    * Biclique support for Pegasus

--- dwave/system/package_info.py
+++ dwave/system/package_info.py
@@ -15,7 +15,7 @@
 # =============================================================================
 __all__ = ['__version__', '__author__', '__authoremail__', '__description__']
 
-__version__ = '0.9.5'
+__version__ = '0.9.6'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'All things D-Wave System.'
