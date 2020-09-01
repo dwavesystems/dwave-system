772134d306c9b8b82835fb86a68c17be19e856eb
Alexander Condello <arcondello@gmail.com>
2020-08-31 17:15:57 -0700

    Fix chain strength calculation in DWaveCliqueSampler to use spin biases

--- dwave/system/samplers/clique.py
+++ dwave/system/samplers/clique.py
@@ -171,21 +171,20 @@ class DWaveCliqueSampler(dimod.Sampler):
 
         assert bqm.num_variables == len(embedding)  # sanity check
 
-        if chain_strength is None:
-            # chain length determines chain strength
-            if embedding and bqm.num_interactions > 0:
-                squared_j = (j ** 2 for j in bqm.quadratic.values())
-                rms = math.sqrt(sum(squared_j)/bqm.num_interactions)
-       
-                chain_strength = 1.5 * rms * math.sqrt(bqm.num_variables)
-            else:
-                chain_strength = 1  # doesn't matter
-
         # scaling only make sense in Ising space
         original_bqm = bqm
 
         if bqm.vartype is not dimod.SPIN:
             bqm = bqm.change_vartype(dimod.SPIN, inplace=False)
+
+        if chain_strength is None:
+            # chain length determines chain strength
+            if embedding and bqm.num_interactions > 0:
+                squared_j = (j ** 2 for j in bqm.quadratic.values())
+                rms = math.sqrt(sum(squared_j)/bqm.num_interactions)
+                chain_strength = 1.5 * rms * math.sqrt(bqm.num_variables)
+            else:
+                chain_strength = 1  # doesn't matter
 
         sampler = FixedEmbeddingComposite(
             dimod.ScaleComposite(self.child),
