89c96b4824cfe93ea43d3a166d532c710c57961f
Akash Narayanan <83135130+AkashNarayanan@users.noreply.github.com>
2021-09-12 21:59:58 +0530

    Update

--- dwave/system/samplers/dwave_sampler.py
+++ dwave/system/samplers/dwave_sampler.py
@@ -376,8 +376,8 @@ class DWaveSampler(dimod.Sampler, dimod.Structured):
         # on missing nodes for lists
         if isinstance(h, list):
             if len(h) > self.solver.num_qubits:
-                msg = "Problem graph incompatible with solver. Please use 'EmbeddingComposite' "
-                      "to map the problem graph to the solver."
+                msg = ("Problem graph incompatible with solver. Please use 'EmbeddingComposite' "
+                      "to map the problem graph to the solver.")
                 raise BinaryQuadraticModelStructureError(msg)
             nodes = self.solver.nodes
             h = dict((v, b) for v, b in enumerate(h) if b and v in nodes)
