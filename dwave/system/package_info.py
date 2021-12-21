15f1328072f0c2febfb50c0d09777ae29d7e4c55
Radomir Stevanovic <radomir@dwavesys.com>
2021-12-21 11:47:00 -0800

    Release 1.11.0

--- dwave/system/package_info.py
+++ dwave/system/package_info.py
@@ -14,7 +14,7 @@
 
 __all__ = ['__version__', '__author__', '__authoremail__', '__description__']
 
-__version__ = '1.10.0'
+__version__ = '1.11.0'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'All things D-Wave System.'
