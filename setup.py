22415fe608fb697d64355f03104289ef6e820106
Tim Doyle <tdoyle@dwavesys.com>
2022-05-20 16:08:59 -0700

    Update minorminer version in setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ install_requires = ['dimod>=0.10.0,<0.13.0',
                     'dwave-preprocessing>=0.3,<0.5',
                     'networkx>=2.0,<3.0',
                     'homebase>=1.0.0,<2.0.0',
-                    'minorminer>=0.2.4,<0.3.0',
+                    'minorminer>=0.2.8,<0.3.0',
                     'numpy>=1.17.3,<2.0.0',
                     'dwave-tabu>=0.4.2',
                     ]
