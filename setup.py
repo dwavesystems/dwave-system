0e62247b4e594bd509ebece2f4e4a0bea76aa072
Alexander Condello <arcondello@gmail.com>
2023-06-07 08:59:08 -0700

    Require dimod>=0.12.7 in install_requires

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ os.chdir(setup_folder_loc)
 exec(open(os.path.join(".", "dwave", "system", "package_info.py")).read())
 
 
-install_requires = ['dimod>=0.12.0,<0.14.0',
+install_requires = ['dimod>=0.12.7,<0.14.0',
                     'dwave-cloud-client>=0.9.1,<0.11.0',
                     'dwave-networkx>=0.8.10',
                     'dwave-preprocessing>=0.5.0',
