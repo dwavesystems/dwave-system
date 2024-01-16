0ac371bcd8eac8330bffd4bb78c8bf6f6f68c5e6
Alexander Condello <arcondello@gmail.com>
2024-01-15 17:21:41 -0800

    Remove NetworkX as a direct dependency
    Closes https://github.com/dwavesystems/dwave-system/issues/504

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ install_requires = ['dimod>=0.12.7,<0.14.0',
                     'dwave-cloud-client>=0.11.0,<0.13.0',
                     'dwave-networkx>=0.8.10',
                     'dwave-preprocessing>=0.5.0',
-                    'networkx>=2.0,<3.0',
                     'homebase>=1.0.0,<2.0.0',
                     'minorminer>=0.2.8,<0.3.0',
                     'numpy>=1.21.6',   # minimum inherited from minorminer
